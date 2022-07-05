f2b59b432a109cf7d97f84677f7d43b035506c56
jonlam1989 <jonlam1989@outlook.com>
2022-07-05 13:12:06 -0400

    initial setup

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+//routes
+
+const PORT = process.env.PORT || 3001;
+const start = () => {
+	try {
+		app.listen(PORT, () => {
+			console.log(`server started on port: ${PORT}`);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();
